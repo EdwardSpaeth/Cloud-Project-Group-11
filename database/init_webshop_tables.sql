4aae07847c051168e1e595cfe392f4bed4ae2c72
jatenderjossan <93328599+jatenderjossan@users.noreply.github.com>
2025-01-23 16:48:58 +0100

    added product table creation code

--- database/init_webshop_tables.sql
+++ database/init_webshop_tables.sql
@@ -5,3 +5,31 @@ CREATE TABLE users (
     email VARCHAR(50) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE Product {
+    prodID          VARCHAR(512),
+    prodName        VARCHAR(100)  NOT NULL,
+    prodPicture     VARCHAR(),
+    prodCategory    VARCHAR(100)  NOT NULL,
+    prodCurrency    VARCHAR(100)  NOT NULL,
+    prodPrice       DECIMAL(7, 2) NOT NULL,
+    prodBrand       VARCHAR(100)  NOT NULL,
+    prodDescription VARCHAR(1000),
+    prodInfo        VARCHAR(1000),
+    prodMaterial    VARCHAR(500),
+    prodColor       VARCHAR(50)   NOT NULL,
+    PRIMARY KEY ('prodID')
+}
+
+INSERT INTO Product (prodID, prodName, prodCategory, prodCurrency, prodPrice, prodBrand, prodDescription, prodInfo, prodMaterial, prodColor)
+VALUES 
+('', 'Modern Sofa', 'Home & Garden', 'Dollar', 999.99, 'Lowtech Hifi', '', '', '', 'Grey'),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', ''),
+('', '', '', '', '', '', '', '', '', '')
