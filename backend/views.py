4ac492e933a75d6b6235c2dad8068687634149bc
Edward Späth <edward5000@gmx.de>
2025-01-26 18:13:00 +0100

    /products/id now retrieves some information about product

--- backend/views.py
+++ backend/views.py
@@ -2,6 +2,32 @@
 from . import app
 from . import db
 from .entities import Product, ProductColor, ProductMaterial, Customer, Order, OrderItem, Supplier, Inventory
+import json
+
+@app.route("/products/<int:id>")
+def get_description(id: int):
+    if id == 0:
+        products_info = []
+        products = Product.query.all()
+        for product in products:
+            products_info.append({
+                "name": product.productName,
+                "category": product.productCategory,
+                "price": str(product.productPrice),
+                "currency": product.productCurrency,
+                "description": product.productDescription
+            })
+        return json.dumps(products_info), 200
+    else:
+        product = Product.query.filter(Product.productID == 1).first()
+        product_info = json.dumps({
+            "name": product.productName,
+            "category": product.productCategory,
+            "price": str(product.productPrice),
+            "currency": product.productCurrency,
+            "description": product.productDescription
+        })
+        return product_info, 200
 
 @app.route("/products/<int:id>")
 def serve_home(id: int):
