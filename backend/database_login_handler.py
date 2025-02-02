f5e54a0c55521466c9bb7c94b96dc212b9493dd4
Edward Späth <edward5000@gmx.de>
2025-02-02 18:21:31 +0100

    Instead of relying on the local .env file, the database connection string is instead read via environment variables

--- backend/database_login_handler.py
+++ backend/database_login_handler.py
@@ -1,16 +1,8 @@
-from dotenv import load_dotenv
 import os
+import urllib
 
 def get_database_connection_string() -> str:
-    # Load environment variables (including DB password) from the .env file located in "secrets" directory.
-    load_dotenv(os.path.abspath(os.path.dirname(__file__)) + os.sep + ".." + os.sep + "secrets" + os.sep + ".env")
-
-    DB_HOST = os.getenv("LOWTECH_DB_HOST")
-    DB_PORT = os.getenv("LOWTECH_DB_PORT")
-    DB_NAME = os.getenv("LOWTECH_DB_NAME")
-    DB_USER = os.getenv("LOWTECH_DB_USER")
-    DB_PASSWORD = os.getenv("LOWTECH_DB_PASSWORD")
-
-    # Create a connection string for SQLAlchemy
-    connection_string = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    driver_string = os.getenv("LOWTECHDATABASECONNECTIONSTRING")
+    params = urllib.parse.quote_plus(driver_string)
+    connection_string = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
     return connection_string
