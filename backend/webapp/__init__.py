b82bd715435c831ea1faa0d14fb7f3535d644518
Edward Späth <edward5000@gmx.de>
2025-02-27 13:14:10 +0100

    Adjusted how timeouts work for DB connections

--- backend/webapp/__init__.py
+++ backend/webapp/__init__.py
@@ -14,12 +14,13 @@ app.config["SQLALCHEMY_DATABASE_URI"] = get_database_connection_string()
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_size": 5,
     "max_overflow": 10,
-    "pool_recycle": 1800,
-    "pool_pre_ping": True
+    "pool_recycle": 900,
+    "pool_pre_ping": True,
+    "pool_timeout": 30
 }
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db: SQLAlchemy = SQLAlchemy(app)
+db: SQLAlchemy = SQLAlchemy(app, engine_options={"pool_recycle": 900, "pool_pre_ping": True})
 with app.app_context():
     db.reflect()
     
